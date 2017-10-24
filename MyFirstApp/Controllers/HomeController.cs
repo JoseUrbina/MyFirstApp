6bc8ff1ad106bba35e522b1df3a938e3470cc11d
Jose Urbina <joseantonioug-09@hotmail.com>
2017-10-24 16:25:35 -0600

    Agregar archivos de proyecto.

---
+++ MyFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "My first Application with ASP.NET MVC 5";
+        }
+    }
+}
